48046d53fb06608e1716b9a7984524534535729d
David De Anda <omarandstuff@gmail.com>
2018-06-26 20:23:28 -0500

    Export full api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import _Pipeline from './Pipeline'
 import _Stage from './Stage'
 import _LocalStep from './LocalStep'
 import _RemoteStep from './RemoteStep'
+import Local from './Local'
+import LocalManager from './LocalManager'
+import Printer from './Printer'
+import Remote from './Remote'
+import RemoteManager from './RemoteManager'
 
 export function Pipeline(title, config, theme) {
   return new _Pipeline(title, config, theme)
@@ -17,4 +22,16 @@ export function LocalStep(definition) {
 
 export function RemoteStep(definition) {
   return new _RemoteStep(definition)
+}
+
+export default {
+  Pipeline: _Pipeline,
+  Stage: _Stage,
+  LocalStep: _LocalStep,
+  RemoteStep: _RemoteStep,
+  Local: Local,
+  LocalManager: LocalManager,
+  Printer: Printer,
+  Remote: Remote,
+  RemoteManager: RemoteManager
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,13 @@ import _Pipeline from '../src/Pipeline'
 import _Stage from '../src/Stage'
 import _LocalStep from '../src/LocalStep'
 import _RemoteStep from '../src/RemoteStep'
+import Local from '../src/Local'
+import LocalManager from '../src/LocalManager'
+import Printer from '../src/Printer'
+import Remote from '../src/Remote'
+import RemoteManager from '../src/RemoteManager'
 import { Pipeline, Stage, LocalStep, RemoteStep } from '../src/index'
+import api from '../src/index'
 
 describe('index', () => {
   it('exports the object creator functions', () => {
@@ -10,5 +16,19 @@ describe('index', () => {
     expect(Stage('', {})).toBeInstanceOf(_Stage)
     expect(LocalStep()).toBeInstanceOf(_LocalStep)
     expect(RemoteStep()).toBeInstanceOf(_RemoteStep)
+  })
+
+  it('exports api component as default', () => {
+    expect(api).toEqual({
+      Pipeline: _Pipeline,
+      Stage: _Stage,
+      LocalStep: _LocalStep,
+      RemoteStep: _RemoteStep,
+      Local: Local,
+      LocalManager: LocalManager,
+      Printer: Printer,
+      Remote: Remote,
+      RemoteManager: RemoteManager
+    })
   })
 })
